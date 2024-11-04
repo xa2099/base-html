9bfa805df14adf4a49bda113a9088f3ebc31a74b
Roman Romanenko <37461099+xa2099@users.noreply.github.com>
2024-11-04 20:46:02 +0000

    .

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
 function toggle(elementID) {
     var el = document.getElementById(elementID);
-    if (el.style.display === 'none') {
+    if (el.style.display !== 'block') {
         el.style.display = 'block';
     } else {
         el.style.display = 'none';
