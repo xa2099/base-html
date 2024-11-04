ff7af4a62930c6863fbdf73e83031e9a7448e4bf
Roman Romanenko <37461099+xa2099@users.noreply.github.com>
2024-11-04 09:45:23 -0500

    Initial.

---
+++ base.js
@@ -0,0 +1,8 @@
+function toggle(elementID) {
+    var el = document.getElementById(elementID);
+    if (el.style.display === 'none') {
+        el.style.display = 'block';
+    } else {
+        el.style.display = 'none';
+    }
+}
